b329156b80701031176106b946a09cfd81b99d66
Dmitri Zagidulin <dzagidulin@gmail.com>
2026-07-31 23:26:36 -0400

    Convert source to TypeScript ESM; build with tsc + serialize script

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const CONTEXT_FILENAME = 'example-v1.jsonld'
+export const CONTEXT_URL_V1 = 'https://w3id.org/example/v1'
+// value between 0x0 and 0x7FFF for globally registered term codec values
+// or >= 0x8000 for app-specific local terms
+export const CBORLD_VALUE = 0x0
